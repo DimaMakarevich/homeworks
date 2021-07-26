2496ff995b3885084792962b8ffc117e6867cc49
DmitryMakarevich <dmitrymakarevich@coherentsolutions.com>
2021-07-26 14:08:43 +0300

    jenkins

--- 6_Java/src/main/java/Config.java
+++ 6_Java/src/main/java/Config.java
@@ -3,7 +3,8 @@ public class Config {
     public static String url = "jdbc:mysql://host.docker.internal:3310/mysql?useLegacyDatetimeCode=false&serverTimezone=UTC";
     public static String driver = "com.mysql.jdbc.Driver";
     public static String username = "root";
-   // public static String password = "Dima1790426";
-   public static String password = "";
-    public static String data_path = "C:\\Users\\DmitryMakarevich\\Desktop\\homeworks\\6_Java\\fund";
+    // public static String password = "Dima1790426";
+    public static String password = "";
+   // public static String data_path = "C:\\Users\\DmitryMakarevich\\Desktop\\homeworks\\6_Java\\fund";
+   public static String data_path = "6_Java/fund";
 }
